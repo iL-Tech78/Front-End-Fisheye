eaaaf833ab7f34a1dd2d72f4cc94beac7120c0cb
IL-iliesse78 <86209795+IL-iliesse78@users.noreply.github.com>
2024-09-24 17:58:00 +0200

    Étape numéros 3 : - Ajout des attributs ARIA pour améliorer l'accessibilité (aria-label). - Ajout de classes pour chaque élément de l'article (carte photographe) dans la fonction photographerFactory. - Ajout des style dans les classes Mise en place des classes suivantes : photographer-card, photographer-card__image, photographer-card__name, photographer-card__location, photographer-card__tagline, photographer-card__price pour ce rapprocher au mieux de la maquette.

--- scripts/templates/photographer.js
+++ scripts/templates/photographer.js
@@ -3,25 +3,37 @@ function photographerFactory(data) {
 
     const picture = `../../assets/photographers/${portrait}`;
 
-    // Création de mon éléments HTML 
+    // Création de mon élément HTML 
     function getUserCardDOM() {
         const article = document.createElement('article');
+        article.setAttribute('aria-label', `Profil de ${name}`);
 
         const img = document.createElement('img');
         img.setAttribute('src', picture);
-        img.setAttribute('alt', name);
+        img.setAttribute('alt', `Portrait de ${name}`);
+        img.classList.add('img__photographer');
+
 
         const h2 = document.createElement('h2');
         h2.textContent = name;
+        h2.classList.add('name__photographer');
+
 
         const location = document.createElement('p');
         location.textContent = `${city}, ${country}`;
+        location.setAttribute('aria-label', `Localisation: ${city}, ${country}`);
+        location.classList.add('location__photographer');
+
 
         const description = document.createElement('p');
         description.textContent = tagline;
+        description.classList.add('description__photographer');
 
         const pricing = document.createElement('p');
         pricing.textContent = `${price}€/jour`;
+        pricing.setAttribute('aria-label', `Prix: ${price} euros par jour`);
+        pricing.classList.add('price__photographer');
+
 
         article.appendChild(img);
         article.appendChild(h2);
@@ -31,6 +43,7 @@ function photographerFactory(data) {
 
         return article;
     }
+
 
     return { name, picture, getUserCardDOM };
 }
